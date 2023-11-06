23bd08d68173e64f8a115ccdd2e3544e32e9677c
ManInBlack-coder <aleksq066@gmail.com>
2023-11-06 16:37:12 +1300

    Lõputöö

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'/')));
+
+app.get('/',async(req,res) => {
+    res.sendFile(path.join(__dirname,'index'));
+
+});
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+})
